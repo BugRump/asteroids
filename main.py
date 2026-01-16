6d21260a8bb48c8b6dabff69f8cd047c94a6254c
Bugrump <edmcdoughty@gmail.com>
2026-01-16 12:24:50 -0700

    B: Finished initial game loop lessons

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")        
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
     
 
 
