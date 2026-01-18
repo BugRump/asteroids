28b5daa21c1ba96553c98c230c1e7831ce1dba2b
BugRump <EDMCDoughty@gmail.com>
2026-01-18 10:58:37 -0700

    D: Player class added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
@@ -12,18 +13,20 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_character = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")        
+        screen.fill("black")
+        player_character.draw(screen)        
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
-    
-
-
 
 if __name__ == "__main__":
     main()
